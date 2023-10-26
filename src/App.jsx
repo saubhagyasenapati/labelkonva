b741102895714294f001522362e43c09fe2941c7
saubhagyasenapati <saubhagya.sena@gmail.com>
2023-10-26 12:25:52 +0530

    Label studio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Canvas from "./SubComponent/Canvas";
+import { Button, Space } from "antd";
+function App() {
+  return (
+    <>
+    
+      <Canvas color />
+    </>
+  );
+}
+
+export default App;
